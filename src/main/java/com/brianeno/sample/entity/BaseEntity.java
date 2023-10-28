639dbebe665565c4b3a7db5ca18d70453535b1df
Enochson <B_Enochson@acs.org>
2023-10-28 10:03:59 -0400

    initial check-in

---
+++ src/main/java/com/brianeno/sample/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.brianeno.sample.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+abstract class BaseEntity implements Serializable {
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
